88106e4d77e3747bcdf446fc1f16b39b9090eeb9
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2021-04-06 12:09:30 -0300

    Corrigindo erro com a seleção do sexo

--- IPIP/bot.py
+++ IPIP/bot.py
@@ -11,109 +11,114 @@ class bot():
 
     def preencherForms(self):
         drive = self.drive
-        drive.get('https://www.personal.psu.edu/~j5j/IPIP/ipipneo120.htm')
-        time.sleep(3)
-        yes1 = drive.find_element_by_xpath('/html/body/form/table[1]/tbody/tr/td/input')
-        yes1.click()
 
-        yes2 = drive.find_element_by_xpath('/html/body/form/table[2]/tbody/tr/td/input')
-        yes2.click()
+        for pessoa in range (83, 121):
+            drive.get('https://www.personal.psu.edu/~j5j/IPIP/ipipneo120.htm')
+            time.sleep(5)
+            yes1 = drive.find_element_by_xpath('/html/body/form/table[1]/tbody/tr/td/input')
+            yes1.click()
 
-        botaoSend = drive.find_element_by_xpath('/html/body/form/b[2]/p/input')
-        botaoSend.click()
-        
-        # esperar clicar no botao de aceitar sem seguranca
-        time.sleep(20)
+            yes2 = drive.find_element_by_xpath('/html/body/form/table[2]/tbody/tr/td/input')
+            yes2.click()
 
-        # A pessoa que está sendo avaliada
-        linhaDaPessoa = 2
-
-        # nome
-        nome = drive.find_element_by_xpath('/html/body/form/p[2]/input')
-        nome.send_keys(self.ws[linhaDaPessoa][0].value)
-
-        # sexo
-        sexo = self.ws[linhaDaPessoa][2].value
-        if (sexo == "Masculino"):
-            sexoMasculino = drive.find_element_by_xpath('/html/body/form/p[3]/table/tbody/tr/td[2]/input')
-            sexoMasculino.click()
-        
-        elif (sexo == "Feminino"):
-            sexoFeminino = drive.find_element_by_xpath('/html/body/form/p[3]/table/tbody/tr/td[1]/input')
-            sexoFeminino.click()
-
-        # idade
-        idade = drive.find_element_by_xpath('/html/body/form/p[4]/input')
-        idade.send_keys(self.ws[linhaDaPessoa][3].value)
-
-        # pais
-        pais = drive.find_element_by_xpath('/html/body/form/p[6]/select')
-        pais.send_keys('Brazil')
-
-        # Parte 1 do formulario
-        for numeroQuestaoFormulario in range(1, 61):
-            numeroQuestaoExcel = numeroQuestaoFormulario + 3
-            respostaDaPessoa = self.ws[linhaDaPessoa][numeroQuestaoExcel].value
-            qfinal = ""
-
-            if (respostaDaPessoa == "Muito Impreciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[3]/center/input')
-                
-            elif (respostaDaPessoa == "Moderadamente Impreciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[4]/center/input')
-
-            elif (respostaDaPessoa == "Nem preciso Nem impreciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[5]/center/input')
-
-            elif (respostaDaPessoa == "Moderadamente Preciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[6]/center/input')
-
-            elif (respostaDaPessoa == "Muito Preciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[7]/center/input')      
+            botaoSend = drive.find_element_by_xpath('/html/body/form/b[2]/p/input')
+            botaoSend.click()
             
-            qfinal.click()
-
-        botaoEnviarParte1 = drive.find_element_by_xpath('/html/body/form/p[7]/input')
-        botaoEnviarParte1.click()        
-        
-        # esperar a transicao entre as telas
-        time.sleep(20)
-
-        # Parte 2 do formulario
-        for numeroQuestaoFormulario in range(1, 61):
-            numeroQuestaoExcel = numeroQuestaoFormulario + 63
-            respostaDaPessoa = self.ws[linhaDaPessoa][numeroQuestaoExcel].value
-            qfinal = ""
-
-            if (respostaDaPessoa == "Muito Impreciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[3]/center/input')          
-                
-            elif (respostaDaPessoa == "Moderadamente Impreciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[4]/center/input')  
-
-            elif (respostaDaPessoa == "Nem preciso Nem impreciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[5]/center/input')  
-
-            elif (respostaDaPessoa == "Moderadamente Preciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[6]/center/input')  
-
-            elif (respostaDaPessoa == "Muito Preciso"):
-                qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[7]/center/input')
+            # esperar clicar no botao de aceitar sem seguranca
             
-            qfinal.click()  
+            botaoSendSeguranca = drive.find_element_by_xpath('//*[@id="proceed-button"]')
+            botaoSendSeguranca.click()
+            time.sleep(5)
 
-        # buffer
-        time.sleep(10)  
+            # linha da planilha 
+            linhaDaPessoa = pessoa
 
-        botaoEnviarParte2 = drive.find_element_by_xpath('/html/body/form/p[3]/input')
-        botaoEnviarParte2.click() 
+            # nome
+            nome = drive.find_element_by_xpath('/html/body/form/p[2]/input')
+            nome.send_keys(self.ws[linhaDaPessoa][0].value)
 
-        # buffer
-        time.sleep(60)  
+            # sexo
+            sexo = self.ws[linhaDaPessoa][2].value
+            if (sexo == "Masculino"):
+                sexoMasculino = drive.find_element_by_xpath('/html/body/form/p[3]/table/tbody/tr/td[2]/input')
+                sexoMasculino.click()
+            
+            elif (sexo == "Feminino"):
+                sexoFeminino = drive.find_element_by_xpath('/html/body/form/p[3]/table/tbody/tr/td[3]/input')
+                sexoFeminino.click()
 
-        texto = drive.find_element_by_xpath('/html/body').text
-        arquivo = open(self.ws[linhaDaPessoa][0].value + ".txt", "a")
-        arquivo.write(str(texto))      
+            # idade
+            idade = drive.find_element_by_xpath('/html/body/form/p[4]/input')
+            idade.send_keys(self.ws[linhaDaPessoa][3].value)
+
+            # pais
+            pais = drive.find_element_by_xpath('/html/body/form/p[6]/select')
+            pais.send_keys('Brazil')
+
+            # Parte 1 do formulario
+            for numeroQuestaoFormulario in range(1, 61):
+                numeroQuestaoExcel = numeroQuestaoFormulario + 3
+                respostaDaPessoa = self.ws[linhaDaPessoa][numeroQuestaoExcel].value
+                qfinal = ""
+
+                if (respostaDaPessoa == "Muito Impreciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[3]/center/input')
+                    
+                elif (respostaDaPessoa == "Moderadamente Impreciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[4]/center/input')
+
+                elif (respostaDaPessoa == "Nem preciso Nem impreciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[5]/center/input')
+
+                elif (respostaDaPessoa == "Moderadamente Preciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[6]/center/input')
+
+                elif (respostaDaPessoa == "Muito Preciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/p[6]/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[7]/center/input')      
+                
+                qfinal.click()
+
+            botaoEnviarParte1 = drive.find_element_by_xpath('/html/body/form/p[7]/input')
+            botaoEnviarParte1.click()        
+            
+            # esperar a transicao entre as telas
+            time.sleep(10)
+
+            # Parte 2 do formulario
+            for numeroQuestaoFormulario in range(1, 61):
+                numeroQuestaoExcel = numeroQuestaoFormulario + 63
+                respostaDaPessoa = self.ws[linhaDaPessoa][numeroQuestaoExcel].value
+                qfinal = ""
+
+                if (respostaDaPessoa == "Muito Impreciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[3]/center/input')          
+                    
+                elif (respostaDaPessoa == "Moderadamente Impreciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[4]/center/input')  
+
+                elif (respostaDaPessoa == "Nem preciso Nem impreciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[5]/center/input')  
+
+                elif (respostaDaPessoa == "Moderadamente Preciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[6]/center/input')  
+
+                elif (respostaDaPessoa == "Muito Preciso"):
+                    qfinal = drive.find_element_by_xpath('/html/body/form/table/tbody/tr['+str(numeroQuestaoFormulario)+']/td[7]/center/input')
+                
+                qfinal.click()  
+
+            # buffer
+            time.sleep(3)  
+
+            botaoEnviarParte2 = drive.find_element_by_xpath('/html/body/form/p[3]/input')
+            botaoEnviarParte2.click() 
+
+            # buffer
+            time.sleep(10)  
+
+            texto = drive.find_element_by_xpath('/html/body').text
+            arquivo = open(str(self.ws[linhaDaPessoa][0].value) + ".txt", "a")
+            arquivo.write(str(texto))      
         
 
 bot = bot()
